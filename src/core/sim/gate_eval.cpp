616187cf0b5be118c3d76d3dda5c150f69765584
ranaumarnadeemgit config --global user.name ranaumarnadeem <ranaumarnadeem632@gmail.com>
2026-06-11 21:05:32 +0500

    feat: extend gate types and evaluation logic for additional gates

--- src/core/sim/gate_eval.cpp
+++ src/core/sim/gate_eval.cpp
@@ -14,6 +14,7 @@ uint64_t eval_bitwise(GateType type, const std::vector<uint64_t>& inputs) {
   const uint64_t in1 = pick(inputs, 1);
   const uint64_t in2 = pick(inputs, 2);
   const uint64_t in3 = pick(inputs, 3);
+  const uint64_t in4 = pick(inputs, 4);
 
   switch (type) {
     case GateType::INV:
@@ -23,20 +24,38 @@ uint64_t eval_bitwise(GateType type, const std::vector<uint64_t>& inputs) {
       return in0;
     case GateType::AND2:
       return in0 & in1;
+    case GateType::AND2B:
+      return in0 & ~in1;
+    case GateType::AND3:
+      return in0 & in1 & in2;
+    case GateType::AND4:
+      return in0 & in1 & in2 & in3;
     case GateType::OR2:
       return in0 | in1;
+    case GateType::OR3:
+      return in0 | in1 | in2;
+    case GateType::OR4:
+      return in0 | in1 | in2 | in3;
     case GateType::NAND2:
       return ~(in0 & in1);
     case GateType::NAND3:
       return ~(in0 & in1 & in2);
+    case GateType::NAND4:
+      return ~(in0 & in1 & in2 & in3);
     case GateType::NOR2:
       return ~(in0 | in1);
     case GateType::NOR3:
       return ~(in0 | in1 | in2);
+    case GateType::NOR4:
+      return ~(in0 | in1 | in2 | in3);
     case GateType::XOR2:
       return in0 ^ in1;
+    case GateType::XOR3:
+      return in0 ^ in1 ^ in2;
     case GateType::XNOR2:
       return ~(in0 ^ in1);
+    case GateType::XNOR3:
+      return ~(in0 ^ in1 ^ in2);
     case GateType::OAI21:
       return ~((in0 | in1) & in2);
     case GateType::AOI21:
@@ -48,6 +67,78 @@ uint64_t eval_bitwise(GateType type, const std::vector<uint64_t>& inputs) {
     // OSU035 MUX2X1: Y = ~((S&A)|(~S&B)); pins A,B,S -> in0,in1,in2
     case GateType::MUX2:
       return ~((in2 & in0) | (~in2 & in1));
+    case GateType::MUX2_NI:
+      return (~in2 & in0) | (in2 & in1);
+    case GateType::A21O:
+      return (in0 & in1) | in2;
+    case GateType::A21OI:
+      return ~((in0 & in1) | in2);
+    case GateType::A21BO:
+      return (in0 & in1) | ~in2;
+    case GateType::A21BOI:
+      return ~((in0 & in1) | ~in2);
+    case GateType::A22O:
+      return (in0 & in1) | (in2 & in3);
+    case GateType::A22OI:
+      return ~((in0 & in1) | (in2 & in3));
+    case GateType::A211OI:
+      return ~((in0 & in1) | in2 | in3);
+    case GateType::A2111OI:
+      return ~((in0 & in1) | in2 | in3 | in4);
+    case GateType::A221O:
+      return (in0 & in1) | (in2 & in3) | in4;
+    case GateType::A221OI:
+      return ~((in0 & in1) | (in2 & in3) | in4);
+    case GateType::A31O:
+      return (in0 & in1 & in2) | in3;
+    case GateType::A31OI:
+      return ~((in0 & in1 & in2) | in3);
+    case GateType::A32O:
+      return (in0 & in1 & in2) | (in3 & in4);
+    case GateType::A32OI:
+      return ~((in0 & in1 & in2) | (in3 & in4));
+    case GateType::A41OI:
+      return ~((in0 & in1 & in2 & in3) | in4);
+    case GateType::O21A:
+      return (in0 | in1) & in2;
+    case GateType::O21AI:
+      return ~((in0 | in1) & in2);
+    case GateType::O21BAI:
+      return ~((in0 | in1) & ~in2);
+    case GateType::O22A:
+      return (in0 | in1) & (in2 | in3);
+    case GateType::O22AI:
+      return ~((in0 | in1) & (in2 | in3));
+    case GateType::O211AI:
+      return ~((in0 | in1) & in2 & in3);
+    case GateType::O221AI:
+      return ~((in0 | in1) & (in2 | in3) & in4);
+    case GateType::O31A:
+      return (in0 | in1 | in2) & in3;
+    case GateType::O31AI:
+      return ~((in0 | in1 | in2) & in3);
+    case GateType::O311A:
+      return (in0 | in1 | in2) & in3 & in4;
+    case GateType::O311AI:
+      return ~((in0 | in1 | in2) & in3 & in4);
+    case GateType::O32AI:
+      return ~((in0 | in1 | in2) & (in3 | in4));
+    case GateType::NAND2B:
+      return in0 | ~in1;
+    case GateType::NAND3B:
+      return in0 | ~in1 | ~in2;
+    case GateType::NAND4B:
+      return in0 | ~in1 | ~in2 | ~in3;
+    case GateType::NOR2B:
+      return ~in0 & in1;
+    case GateType::NOR3B:
+      return ~in0 & ~in1 & in2;
+    case GateType::NOR4B:
+      return ~in0 & ~in1 & ~in2 & in3;
+    case GateType::OR3B:
+      return in0 | in1 | ~in2;
+    case GateType::OR4B:
+      return in0 | in1 | in2 | ~in3;
     case GateType::CONST0:
       return 0ULL;
     case GateType::CONST1:
